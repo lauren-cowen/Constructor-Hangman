7652229f1021dd9db35d93b821f3e5fcfecdb609
Lauren Cowen <lauren.cowen@gmail.com>
2017-10-19 00:30:58 -0500

    done

--- constructorhangman/main.js
+++ constructorhangman/main.js
@@ -1,102 +1,68 @@
-// var letter = require('letter');
 var word = require('./word.js');
 var letter = require('./letter');
 var inquirer = require('inquirer');
+var storage = ["harry", "ron", "hermione", "hedwig", "hagrid", "snape", "voldemort", "dumbledore", "malfoy", "mcgonagall"];
 
-// var hangmanGame = function(callback) {
-
-// 	var wordtoguess;
-// 	var playAgain = false;
-// 	var newGame = true;
-
-// 	if(newGame) {
-// 		callback (
-// 				pickWord();
-// 			)
-// 	}
-
-// 	else{
-// 		callback(
-
-// 			)
-// 	}
-
-	
-
-// 	pickWord();
-	
-
-
-
-// }
-
-// hangmanGame();
-
-
-// var guessALetter = function (callback) {
-// 		console.log("GuessALetter");
-// 		var letterGuessed = new letter(wordtoguess, process.argv[2]);
-// 		if(letterGuessed.validInput) {
-// 			letterGuessed.compare();
-// 			wordtoguess.printToScreen();
-// 		 }
-// 		 if(wordtoguess.guessesLeft>0 && wordtoguess.spaces.indexOf("_") != -1) {
-// 		callback(
-// 			guessALetter();
-// 			)
-// 		 }
-
-// 	 else{
-	 	
-// 	 	console.log("Would you like to play again?");
-// 	 	if(process.argv[2]==="yes") {
-// 	 		callback(
-// 	 			pickWord();
-// 	 		)
-
-// 	 	};
-
-
-// 		 }
-// 	}
-
-var pickWord = function() {
-		wordtoguess = new word("harry");
-		wordtoguess.blanks();
-		wordtoguess.printToScreen();
-		return word;
-	}
-
-var guessLetter = function(wordtoguess, letterGuessed) {
-	if(letterGuessed.validInput) {
-		letterGuessed.compare();
-		wordtoguess.printToScreen();
-	}
-} 
-
-
-var hangmanGame = function(callback) {
-	
-	var wordtoguess = callback();
-	console.log(JSON.stringify(wordtoguess));
-	var playAgain = false;
-	
-	inquirer.prompt([
-		{
-			type: "input",
-			name: 'userGuess',
-			message: 'Guess a letter'
-		}
-
-		]).then(function(answers) {
-
-			var newLetter = new letter(wordtoguess, answers.userGuess);
-			guessLetter(wordtoguess, newLetter);
-
-
-
-	})
+function getRandomItem(list) {
+    return list[Math.floor(Math.random() * list.length)];
 }
 
-hangmanGame(pickWord);
+var hangmanGame = function() {
+    pickWord();
+    if (wordtoguess.guessesLeft > 0 && wordtoguess.spaces.indexOf("_") != -1) {
+        LetterGuessCallback();
+    }
+}
 
+var pickWord = function() {
+    wordtoguess = new word(getRandomItem(storage));
+    wordtoguess.blanks();
+    wordtoguess.printToScreen();
+
+}
+
+var guessLetter = function(wordtoguess, letterGuessed) {
+    if (letterGuessed.validInput) {
+        letterGuessed.compare();
+        wordtoguess.printToScreen();
+        console.log("Guesses Remaining: " + wordtoguess.guessesLeft)
+    } else {
+        console.log("invalid input");
+    }
+}
+
+var LetterGuessCallback = function(answers) {
+    if (wordtoguess.guessesLeft > 0 && wordtoguess.spaces.indexOf("_") != -1) {
+        inquirer.prompt([{
+                type: "input",
+                name: 'userGuess',
+                message: 'Guess a letter'
+
+            }
+
+        ]).then(function(answers, callback) {
+
+            var newLetter = new letter(wordtoguess, answers.userGuess);
+            guessLetter(wordtoguess, newLetter);
+            LetterGuessCallback();
+
+        })
+    } else {
+        inquirer.prompt([{
+                type: 'input',
+                name: 'playAgain',
+                message: 'Would you like to play again?'
+            }
+
+        ]).then(function(answers) {
+            if (answers.playAgain === "yes") {
+                hangmanGame(pickWord);
+            }
+        })
+    }
+
+
+
+}
+
+hangmanGame();
--- constructorhangman/word.js
+++ constructorhangman/word.js
@@ -1,25 +1,24 @@
 var letter = require('./letter')
 
 module.exports = word = function(guessWord) {
-	this.guessWord = guessWord;
+    this.guessWord = guessWord;
 
-	this.spaces =[];
-	this.progressWord = "";
-	this.guessesLeft = 10;
+    this.spaces = [];
+    this.progressWord = "";
+    this.guessesLeft = 10;
 
 
-	this.blanks = function() {
-		for(i=0;i<this.guessWord.length; i++) {
-			this.spaces.push("_");
-		}
+    this.blanks = function() {
+        for (i = 0; i < this.guessWord.length; i++) {
+            this.spaces.push("_");
+        }
 
-		this.progressWord = this.spaces.join(" ");
+        this.progressWord = this.spaces.join(" ");
 
-	}
+    }
 
-	this.printToScreen = function() {
-		console.log(this.progressWord);
-		
-		}
+    this.printToScreen = function() {
+        console.log(this.progressWord);
+
+    }
 }
-
